701f5d3b50ffa3c069ce2a9f8159c6d37c769cd3
capprice222 <zapolechka5@gmail.com>
2020-12-30 13:20:06 +0700

    Mr. Croooowleeey

---
+++ app/src/main/java/com/example/zapolskiy_v_8/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.zapolskiy_v_8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView home;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        home = findViewById(R.id.home);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
